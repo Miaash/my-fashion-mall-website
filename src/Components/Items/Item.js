891bff7e86b7fd978c0f8b09ffc43344a7228772
seonga <girl0930h@naver.com>
2022-08-18 01:10:27 +0900

    장바구니버튼 추가

--- src/Components/Items/Item.js
+++ src/Components/Items/Item.js
@@ -1,13 +1,15 @@
 import classes from "./Item.module.css";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Item = (props) => {
     return (
         <div className={classes.item}>
             <img src="img/린넨 미디 원피스.png" alt="원피스사진"/>
-            <p className={classes.title}>{props.title}</p>
-            <p className={classes.price}>
+            <span className={classes.icon}><FaShoppingCart className={classes.cartIcon}/></span>
+            <span className={classes.title}>{props.title}</span>
+            <span className={classes.price}>
                 {props.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            </p>
+            </span>
         </div>
     )
 }
