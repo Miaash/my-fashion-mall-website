0ef44667c2df5f3e5406a4666ec362dd0e0c407d
seonga <girl0930h@naver.com>
2022-08-18 01:08:13 +0900

    미디어쿼리작성

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,10 @@ import { FaBars, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 const Header = () => {
     return(
-        <nav>
+        <nav className={classes.header}>
             <ul className={classes.icons}>
                 <li><FaBars className={classes.menubarIcon}/></li>
-                <li><img src="img/mainlogo.png" className={classes.mainlogo}/></li>
+                <li><img src="img/mainlogo.png" className={classes.mainlogo} alt="로고이미지"/></li>
                 <li><FaShoppingCart className={classes.cartIcon}/></li>
                 <li><FaUser className={classes.userIcon}/></li>
             </ul>
